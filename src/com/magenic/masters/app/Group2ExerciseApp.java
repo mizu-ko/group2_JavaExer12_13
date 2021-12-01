920cdc0f2fc901d77752e56ddc1d78b9a5cd9399
Marionino.obog <obog.maironin@gmail.com>
2021-12-01 17:54:58 +0800

    commit

--- src/com/magenic/masters/app/Group2ExerciseApp.java
+++ src/com/magenic/masters/app/Group2ExerciseApp.java
@@ -1,17 +1,64 @@
 package com.magenic.masters.app;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import com.magenic.masters.object.Stock;
 
 public class Group2ExerciseApp {
 
 	public static void main(String[] args) throws IOException {
+		Integer categoryInt = 1;
 		String rawData = Files.readString(Path.of("input/stocks.csv"));
-		System.out.println(rawData);
-		List<Stock> stockItems = rawData.lines().map(a -> {
-			a.split(",");
-		});
+		List<Stock> stockItems = rawData.lines().filter(Predicate.not(String::isBlank)).map(a -> mapToObject(a))
+				.collect(Collectors.toList());
+
+		printCategory();
+		printItemsInCategory(stockItems, getCategory(categoryInt));
+	}
+
+	private static Stock mapToObject(String str) {
+		String[] split = str.split(",");
+		Stock stock = new Stock();
+		stock.setProductName(split[0]);
+		stock.setPrice(BigDecimal.valueOf(Double.valueOf(split[1])));
+		stock.setQuantityType(split[2]);
+		stock.setCategory(split[3]);
+		return stock;
+	}
+
+	private static void printItemsInCategory(List<Stock> stockItems, String category) {
+		Predicate<Stock> filterByCategory = (s) -> s.getCategory().equals(category);
+		Integer n = 0;
+		stockItems.stream()
+		.filter(filterByCategory)
+		.collect(Collectors.toList());
+
+	}
+
+	private static void printCategory() {
+		String categories = """
+				Category:
+				  1 - Pantry
+				  2 - Meat
+				  3 - Snacks
+				""";
+		System.out.println(categories);
+	}
+
+	private static String getCategory(Integer input) {
+		var category = switch (input) {
+		case 0 -> "Pantry";
+		case 1 -> "Meat/Poultry/Seafood";
+		case 2 -> "Snacks";
+		default -> "N/A";
+		};
+		return category;
 	}
 
 }
--- src/com/magenic/masters/object/Stock.java
+++ src/com/magenic/masters/object/Stock.java
@@ -3,9 +3,50 @@ package com.magenic.masters.object;
 import java.math.BigDecimal;
 
 public class Stock {
+	private Integer index;
 	private String productName;
 	private BigDecimal price;
 	private String quantityType;
 	private String category;
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public Integer getIndex() {
+		return index;
+	}
+
+	public void setIndex(Integer index) {
+		this.index = index;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getQuantityType() {
+		return quantityType;
+	}
+
+	public void setQuantityType(String quantityType) {
+		this.quantityType = quantityType;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
 
 }
